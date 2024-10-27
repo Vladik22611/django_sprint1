427cb5b75c692fc00ef805f2c83d9405da33921b
Grechkin Vladislav <vladik.grechkin22@gmail.com>
2024-10-27 10:31:48 +0300

    out static

---
+++ blogicum/pages/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+
+def about(request):
+    template = "pages/about.html"
+    return render(request, template)
+
+
+def rules(request):
+    template = "pages/rules.html"
+    return render(request, template)
+
+
+# Create your views here.
